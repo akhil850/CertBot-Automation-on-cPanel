fbf50fdfedb3e56c14754074b641c0310ec0e713
Akhil S Varughese <49130269+akhil850@users.noreply.github.com>
2020-07-03 17:51:27 +0530

    Create Singular.sh

---
+++ Singular.sh
@@ -0,0 +1,17 @@
+combined=pvtkey,cert,chain
+certificates=cert,chain
+
+============
+cert=cert
+chain=chain
+fullchain=cert,chain
+
+
+
+#!/bin/bash
+
+
+
+new_combined_path='/etc/letsencrypt/live/transport.telangana.gov.in/combined'
+old_combined_path='/var/cpanel/ssl/apache_tls/transport.telangana.gov.in/combined'
+
